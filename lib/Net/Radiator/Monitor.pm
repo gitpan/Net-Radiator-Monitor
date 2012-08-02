01323813029600402e48369cf502acd1ec17800e
Luke Poskitt <ltp@cpan.org>
2012-08-02 01:43:03 +0000

    Import of LTP/Net-Radiator-Monitor-0.021 from CPAN.
    gitpan-cpan-distribution: Net-Radiator-Monitor
    gitpan-cpan-version:      0.021
    gitpan-cpan-path:         LTP/Net-Radiator-Monitor-0.021.tar.gz
    gitpan-cpan-author:       LTP
    gitpan-cpan-maturity:     released

--- lib/Net/Radiator/Monitor.pm
+++ lib/Net/Radiator/Monitor.pm
@@ -7,7 +7,7 @@ use IO::Socket::INET;
 use IO::Select;
 use Carp qw(croak);
 
-our $VERSION = '0.02';
+our $VERSION = '0.021';
 our %METHODS = (
 		server_stats	=> {	cmd => 'STATS',
 					args=> '.',
@@ -41,7 +41,7 @@ sub new {
 	$self->{user}	= $args{user};
 	$self->{passwd}	= $args{passwd};
 	$self->{server}	= (defined $args{server} ? $args{server} : croak 'Constructor failed: no server supplied');
-	$self->{port}   = $args{port} ||= 9084;
+	$self->{port}   = $args{port} ||= 9048;
 	$self->{timeout}= $args{timeout} ||= 5;
 	$self->{sock}= IO::Socket::INET->new(	PeerAddr	=> $self->{server},
 						PeerPort	=> $self->{port},
